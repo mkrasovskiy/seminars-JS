31d357d106813af6511f2d9a84e9e635453882de
Maks <kpasowsky@gmail.com>
2023-04-23 12:28:12 +0300

    lesson1

---
+++ lesson1/script.js
@@ -0,0 +1,43 @@
+console.log("Текст из script.js");
+
+const num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+const b = 1 + 2 + 3;
+console.log(b);
+
+// Однострочный комментарий Ctrl + /!
+/* Многострочный комментарий alt + shift + A */
+
+const c = 10;
+const d = 2;
+const sum = c + d;
+console.log(sum);
+
+const difference = c - d;
+console.log(difference);
+
+const division = c / d;
+console.log(division);
+
+const total = sum + difference + division;
+console.log(total);
+
+const e = 1.5;
+const h = 0.75;
+console.log(e + h);
+
+const g = 100;
+console.log(-g);
+
+const firstName = 'Макс';
+const lastName = 'Красовский';
+console.log(firstName, lastName);
+
+const java = 'Java';
+const script = 'Script';
+console.log(java + script);
