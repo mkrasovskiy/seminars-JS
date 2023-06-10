8b264d99679332d11981d1d3823bbba1650eef4a
Maks <kpasowsky@gmail.com>
2023-06-10 16:23:55 +0300

    Lesson10

---
+++ lesson10/data.js
@@ -0,0 +1,33 @@
+const dataInfo = `
+[
+    {
+        "activity": "Match your storage containers with their lids",
+        "type": "busywork",
+        "key": "4940907"
+    },
+    {
+        "activity": "Pull a harmless prank on one of your friends",
+        "type": "social",
+        "key": "1288934"
+    },
+    {
+        "activity": "Make a new friend",
+        "type": "social",
+        "key": "1000000"
+    },
+    {
+        "activity": "Read a formal research paper on an interesting subject",
+        "type": "education",
+        "key": "3352474"
+    },
+    {
+        "activity": "Fix something that's broken in your house",
+        "type": "diy",
+        "key": "6925988"
+    }
+]
+`
+
+
+
+        
