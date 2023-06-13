6fbc1dedddf01d0fda5c30c85552aa10dd42dd57
Maks <kpasowsky@gmail.com>
2023-06-13 22:08:38 +0300

    lesson 11

---
+++ lesson11/data.js
@@ -0,0 +1,20 @@
+const dataProducts = `
+[
+    {
+        "image": "foto1.jpg",
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "color": "red",
+        "price": "300",
+        "size": "Xl",
+        "Quantity": "2"
+    },
+    {
+        "image": "foto2.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "color": "Black",
+        "price": "52",
+        "size": "Xl",
+        "Quantity": "1"
+    }
+]
+`
